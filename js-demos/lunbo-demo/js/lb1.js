6179370d7d8d56875034cfc3de8b14f6f897c223
Ry-yuan <www.1094324110@qq.com>
2017-08-26 22:51:10 +0800

    lazyload-demo

--- js-demos/lunbo-demo/js/lb1.js
+++ js-demos/lunbo-demo/js/lb1.js
@@ -4,61 +4,63 @@ var arrowRight = document.getElementById("arrowRight");
 // 获得图片容器对象
 var container = document.getElementById("md-container");
 // move变量用于记录当前图片的位置，偏移量
-var offset=0;
-// 为两个按钮添加click事件
+var offset = 0;
+
 var pointBox = document.getElementById("point-box");
 var points = pointBox.getElementsByTagName("li");
 var index = 0;
 // console.log(points.length);
-arrowLeft.addEventListener("click",function(arrowType){movePicture("arrowLeft")},false);
-arrowRight.addEventListener("click",function(arrowType){movePicture("arrowRight")},false);
+// 为两个按钮添加click事件
+arrowLeft.addEventListener("click", function(arrowType) { movePicture("arrowLeft") }, false);
+arrowRight.addEventListener("click", function(arrowType) { movePicture("arrowRight") }, false);
 
-arrowLeft.addEventListener("click",function(){index--;movePoint()},false);
-arrowRight.addEventListener("click",function(){index++;movePoint()},false);
+arrowLeft.addEventListener("click", function() { index--;
+    movePoint() }, false);
+arrowRight.addEventListener("click", function() { index++;
+    movePoint() }, false);
 // 定时器：setInterval()
-var Interval = setInterval("arrowRight.click()",3000);
-container.onmouseover = function(){
-	clearInterval(Interval);
+var Interval = setInterval("arrowRight.click()", 3000);
+container.onmouseover = function() {
+    clearInterval(Interval);
 }
-container.onmouseout = function(){
-	Interval = setInterval("arrowRight.click()",3000);
-}
-// 图片移动函数，参数箭头类型，用于分辨左右箭头
-function movePicture(arrowType){
-	var dp = document.getElementById("demo1Panel");
-	var picLists = dp.getElementsByTagName("li");
-	// 左箭头时执行
-	// 判断是否是最前一张
-	if(arrowType == "arrowRight"){
-		if(offset==(picLists.length-1)*-1000){
-			offset = 0;
-		}
-		else{
-			offset -=1000;	
-		}
-		
-	}
-	// 右箭头时执行
-	// 判断是否到了最后一张
-	if(arrowType == "arrowLeft"){
-		if(offset == 0){
-			offset = (picLists.length-1)*-1000;
-		}
-		else{
-			offset +=1000;
-		}
-	}
-	// 通过改变ul框的left来实现移动
-	dp.style.left=offset+"px";
-}
-function movePoint(){
-	if(index>points.length-1) index=0;
-	else if(index<0) index = points.length-1;
+container.onmouseout = function() {
+        Interval = setInterval("arrowRight.click()", 3000);
+    }
+    // 图片移动函数，参数箭头类型，用于分辨左右箭头
+function movePicture(arrowType) {
+    var dp = document.getElementById("demo1Panel");
+    var picLists = dp.getElementsByTagName("li");
+    // 左箭头时执行
+    // 判断是否是最前一张
+    if (arrowType == "arrowRight") {
+        if (offset == (picLists.length - 1) * -1000) {
+            offset = 0;
+        } else {
+            offset -= 1000;
+        }
 
-	for(var i = 0 ; i<points.length;i++){
-		points[i].style.backgroundColor = "#ccc";
-	}
-	points[index].style.backgroundColor = "#f58220";
+    }
+    // 右箭头时执行
+    // 判断是否到了最后一张
+    if (arrowType == "arrowLeft") {
+        if (offset == 0) {
+            offset = (picLists.length - 1) * -1000;
+        } else {
+            offset += 1000;
+        }
+    }
+    // 通过改变ul框的left来实现移动
+    dp.style.left = offset + "px";
+}
+
+function movePoint() {
+    if (index > points.length - 1) index = 0;
+    else if (index < 0) index = points.length - 1;
+
+    for (var i = 0; i < points.length; i++) {
+        points[i].style.backgroundColor = "#ccc";
+    }
+    points[index].style.backgroundColor = "#f58220";
 }
 
 /*本次收获：
