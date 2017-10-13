baab88baa8d54f637bfa86360a1d27cd2796f47b
ry-yuan <1094324110@qq.com>
2017-10-13 10:22:02 +0800

    [upload] css-demo

--- css-demos/layout/gulpfile.js
+++ css-demos/layout/gulpfile.js
@@ -1,19 +1,19 @@
 // 引入gulp
-var gulp=require('gulp');
+var gulp = require('gulp');
 // 引入gulpl-ess组件
-var less=require('gulp-less');
+var less = require('gulp-less');
 // var cssmin=require('gulp-clean-css');
 // var sourcemaps=require('gulp-sourcemaps');
 // 浏览器同步
 var browserSync = require('browser-sync').create();
-var reload      = browserSync.reload;
+var reload = browserSync.reload;
 // 静态服务器 + 监听 less/html 文件
 gulp.task('serve', ['less'], function() {
     browserSync.init({
-    	// 静态服务器
+        // 静态服务器
         server: "./"
-        // 代理
-        // proxy:'localhost/gulptest'
+            // 代理
+            // proxy:'localhost/gulptest'
     });
     gulp.watch("./less/*.less", ['less']);
     gulp.watch("./*.html").on('change', reload);
@@ -26,7 +26,7 @@ gulp.task('less', function() {
     return gulp.src("./less/*.less")
         .pipe(less())
         .pipe(gulp.dest("./css"))
-        .pipe(reload({stream: true}));
+        .pipe(reload({ stream: true }));
 });
 
 gulp.task('default', ['serve']);
