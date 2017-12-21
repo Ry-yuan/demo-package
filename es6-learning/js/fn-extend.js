fdfb7875bd27bff3381849ac8d7adc0d5eb73cb7
ry-yuan <1094324110@qq.com>
2017-12-21 22:24:46 +0800

    es学习代码

---
+++ es6-learning/js/fn-extend.js
@@ -0,0 +1,59 @@
+// 函数对象扩展
+//1.允许默认值：es5时用的是||来变通，表示默认值：如y = y || 'world';
+//es6允许了默认：
+function fn(x, y = ' world'){
+    console.log(x+y);
+}
+fn('hello');  //'hello world'
+
+//2.rest参数  形式： ...变量名
+//获取函数的多余参数，这样就不需要使用arguments对象
+function a (...val){
+    for(var i of val){
+        console.log(i);
+    }
+}
+a(12,2,1,21,1);  //12,2,1,21,1
+
+//3.ES2016 做了一点修改，规定只要函数参数使用了
+//默认值、解构赋值、或者扩展运算符，那么函数内部就不能显式设定为严格模式,否则会报错
+
+
+//4.name 属性获得函数名
+function foo(){
+}
+fn.name;  //"foo"
+
+
+//5.箭头函数
+//ES6 允许使用“箭头”（=>）定义函数。
+function sum (n1,n2){
+    return n1+n2;
+}
+//箭头函数表示
+var sum = (n1,n2) => n1+n2;
+
+//如果箭头函数的函数体多于一条语句 ， 用花括号括起来
+var fn = (n1,n2) => {var n = 100; return n1+n2+n;}
+
+//如果返回的是对象，用圆括号括起来
+let getObj = (id)  => ({id:id,name:'jack'});
+
+
+//箭头函数的一个用处是简化回调函数
+[1,2,3].map(function(x){
+        return x*2;
+    }
+)
+//箭头写法
+[1,2,3].map(x => x*2);
+
+// 正常函数写法
+var result = values.sort(function (a, b) {
+  return a - b;
+});
+
+// 箭头函数写法
+var result = values.sort((a, b) => a - b);
+
+//箭头函数的this指向的是它外层函数的this，也就是说它的没有自己的this
---
+++ es6-learning/js/string-extend.js
@@ -0,0 +1,18 @@
+//String对象扩展：
+//includes()某个字符串是否包含另一个字符串
+var s = 'hello';
+s.includes('he');  //true
+//startsWith()某个字符串是否一另一个字符串开头
+s.startsWith('hell');  //true
+//endsWith()是否已某个字符串结尾 
+s.endsWith('l');  //false
+//repeat 将某个字符串重复n次,小于0或非数字都是0次
+s.repeat(3);   //hellohellohello
+//padStart()， padEnd() 在头或尾部补全长度
+'ss'.padStart(6,'a');  //'aaaass'  用a从头部开始补全到6长度
+//模板字符串${val}
+//反引号（`）标识。它可以当作普通字符串使用，也可以用来定义多行字符串，或者在字符串中嵌入变量
+var num = 12;
+`the num is ${num*2} `  //"the num is 24"   大括号中可以放表达式
+
+//还有模板编译没搞懂
